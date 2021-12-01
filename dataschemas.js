e213b32c048b64a7c10e8c295dc3f5474dab8458
yuridanil <33320745+yuridanil@users.noreply.github.com>
2021-12-01 17:11:20 +0300

    Add files via upload

---
+++ dataschemas.js
@@ -0,0 +1,32 @@
+export const FLICKR_SCHEMA = {
+  ds_name: ["My Flickr dataset. Page#: ", "photos.page", ", total pages: ", "photos.total"],
+  ds_desc: ["Photos per page: ", "photos.perpage"],
+  items: "photos.photo",
+  item_id: "id",
+  item_title: "title",
+  item_desc: "description._content",
+  item_thumbnail: "url_q",
+  item_src: "url_l"
+};
+
+export const PIXABAY_SCHEMA = {
+  ds_name: ["My Pixabay dataset. Total: ", "total"],
+  ds_desc: ["Total hits: ", "totalHits"],
+  items: "hits",
+  item_id: "id",
+  item_title: "title",
+  item_desc: "tags",
+  item_thumbnail: "previewURL",
+  item_src: "largeImageURL"
+};
+
+export const PEXELS_SCHEMA = {
+  ds_name: ["My Pexels dataset. Page: ", "page"],
+  ds_desc: ["Per page: ", "per_page", " Total results: ", "total_results"],
+  items: "photos",
+  item_id: "id",
+  item_title: "photographer",
+  item_desc: "url",
+  item_thumbnail: "src.tiny",
+  item_src: "src.original"
+};
---
+++ datasets.js
@@ -0,0 +1,52 @@
+function getProperty(object, prop) {
+  return prop.split('.').reduce((acc, part) => acc && acc[part], object);
+}
+
+class Dataset {
+
+  draw = (container) => {
+    container.innerHTML = null;
+    this.items.forEach (item => {
+
+      let img = document.createElement("img");
+      img.src = item.thumbnail;
+
+      let a = document.createElement("a");
+      a.href = item.src;
+      a.target = "blank";
+      a.title = item.desc;
+      a.appendChild(img);
+
+      let p = document.createElement("p");
+      p.innerHTML = item.title;
+
+      let div = document.createElement("div");
+      div.appendChild(a);
+      div.appendChild(p);
+
+      container.appendChild(div);
+    });
+  }
+
+  constructor(object, schema) {
+    this.name = "";
+    this.desc = "";
+    schema.ds_name.forEach(e => { this.name += getProperty(object, e); });
+    schema.ds_desc.forEach(e => { this.desc += getProperty(object, e); });
+    this.items = getProperty(object, schema.items).map(
+      i => new Item(i, schema)
+    );
+  }
+}
+
+class Item {
+  constructor(object, schema) {
+    this.id = getProperty(object, schema.item_id);
+    this.title = getProperty(object, schema.item_title);
+    this.desc = getProperty(object, schema.item_desc);
+    this.thumbnail = getProperty(object, schema.item_thumbnail);
+    this.src = getProperty(object, schema.item_src);
+  }
+}
+
+export {Dataset}
